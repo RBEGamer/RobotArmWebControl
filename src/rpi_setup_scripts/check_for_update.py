f8fb24a820f0db519e3eda68b1f288569231eba0
Marcel Ochsendorf <marcel.ochsendorf@gmail.com>
2019-05-30 22:11:06 +0200

    Update check_for_update.py

--- src/rpi_setup_scripts/check_for_update.py
+++ src/rpi_setup_scripts/check_for_update.py
@@ -1,7 +1,7 @@
 from subprocess import check_output
 
-scanoutput = check_output(["iwlist", "wlan0", "scan"])
-
+scanoutput = check_output(["iwgetid"])
+print scanoutput
 for line in scanoutput.split():
   if line.startswith("ESSID"):
     ssid = line.split('"')[1]
