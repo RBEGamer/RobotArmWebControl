aeec11095ef137d55e25fd030047e0b4a7349fb8
Marcel Ochsendorf <marcel.ochsendorf@gmail.com>
2019-05-31 03:45:55 +0200

    Update install_requirements.sh

--- src/flask_app/install_requirements.sh
+++ src/flask_app/install_requirements.sh
@@ -1,7 +1,11 @@
 #!bin/bash
-cd ./assets
+
+cd ~/RobotArmWebControl/src/flask_app/assets/
+
 bower install
+
 git clone https://github.com/mrdoob/three.js.git
+
 cd..
 
 pip3 install -r requirements.txt
