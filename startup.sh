6286dac66714fe6fe6bd095b26c6306d67ad780b
Marcel Ochsendorf <marcel.ochsendorf@gmail.com>
2019-05-31 02:07:05 +0200

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,8 +1,6 @@
 #!bin/bash
-
+cd /home/pi/RobotArmWebControl
 #CHECK FOR UPDATE
 python ./src/rpi_setup_scripts/check_for_update.py >> ~/Desktop/update_log.txt
-
-
-
+# RUN FLASK APP
 bash ./src/flask_app/run_flask_app.sh &
