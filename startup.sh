5aa3790bfa731a2b22bb6df5b16b7b4c5df02244
Marcel Ochsendorf <marcel.ochsendorf@gmail.com>
2019-05-31 03:10:44 +0200

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,31 +1,27 @@
+#!/bin/sh
+
+if [ ! -f /tmp/RAWCRUNNING.dummy ]; then
+touch /tmp/RAWCRUNNING.dummy
+
+
 cd /home/pi/RobotArmWebControl
 #CHECK FOR UPDATE
 
 
-ssid = $(iw dev | grep ssid | awk '{print $2}')
-if [ "$ssid" = "Keunecke 2" ]; then
-        git stash save --keep-index --include-untracked  
-        git pull
-        bash ./src/rpi_setup_scripts/upload_arduino_sketch.sh
+ssid=$(iw dev | grep ssid | awk '{print $2}')
+if [ "$ssid" = "Keunecke2" ]; then
+git stash save --keep-index --include-untracked  
+ git pull
+   bash ./src/rpi_setup_scripts/upload_arduino_sketch.sh
 fi
 
 if [ "$ssid" = "Keunecke" ]; then
-        git stash save --keep-index --include-untracked  
-        git pull
-        bash ./src/rpi_setup_scripts/upload_arduino_sketch.sh
+git stash save --keep-index --include-untracked  
+ git pull
+   bash ./src/rpi_setup_scripts/upload_arduino_sketch.sh
 fi
-
-if [ "$ssid" = "FRITZBox7362SL" ]; then
-        git stash save --keep-index --include-untracked  
-        git pull
-        bash ./src/rpi_setup_scripts/upload_arduino_sketch.sh
-fi
-
 
 # RUN FLASK APP
-bash ./src/flask_app/run_flask_app.sh &
+bash ./src/flask_app/run_flask_app.sh
 
-
-
-
-
+fi
