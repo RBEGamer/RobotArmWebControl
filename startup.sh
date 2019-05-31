090511b90f236e62f29b61c202ff354a74d05187
Marcel Ochsendorf <marcel.ochsendorf@gmail.com>
2019-05-31 03:40:56 +0200

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -10,15 +10,17 @@ cd /home/pi/RobotArmWebControl
 
 ssid=$(iw dev | grep ssid | awk '{print $2}')
 if [ "$ssid" = "Keunecke2" ]; then
-git stash save --keep-index --include-untracked  
+ git stash save --keep-index --include-untracked  
  git pull
-   bash ./src/rpi_setup_scripts/upload_arduino_sketch.sh
+ bash ./src/flask_app/install_requirements.sh
+ bash ./src/rpi_setup_scripts/upload_arduino_sketch.sh
 fi
 
 if [ "$ssid" = "Keunecke" ]; then
-git stash save --keep-index --include-untracked  
+ git stash save --keep-index --include-untracked  
  git pull
-   bash ./src/rpi_setup_scripts/upload_arduino_sketch.sh
+ bash ./src/flask_app/install_requirements.sh
+ bash ./src/rpi_setup_scripts/upload_arduino_sketch.sh
 fi
 
 # RUN FLASK APP
