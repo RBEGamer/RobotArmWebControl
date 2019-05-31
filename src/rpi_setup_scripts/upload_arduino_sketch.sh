fe2dbfa54dcd6e36aa0883aa069616e0f268f18d
Marcel Ochsendorf <marcel.ochsendorf@gmail.com>
2019-05-31 10:09:29 +0200

    Update upload_arduino_sketch.sh

--- src/rpi_setup_scripts/upload_arduino_sketch.sh
+++ src/rpi_setup_scripts/upload_arduino_sketch.sh
@@ -1,7 +1,18 @@
 #!bin/bash
-echo "-- UPLOADING TO ARDUINO --"
+
 cd ~/RobotArmWebControl/src/
+
+if [ ! -f ./arduino_controller/arduino_controller.ino ]; then
+
+echo "-- UPLOADING TO ARDUINO --"
 arduino-cli board list
 arduino-cli compile --fqbn arduino:sam:arduino_due_x_dbg ./arduino_controller/
 arduino-cli upload -p /dev/ttyACM0 --fqbn arduino:sam:arduino_due_x_dbg ./arduino_controller/
 echo "-- END UPLOADING TO ARDUINO --"
+
+else
+
+echo " -- no arduino_controller.ino in dir --"
+
+fi
+
