1567206c86e85cf95641f35d044b7f0becded3c2
Marcel Ochsendorf <marcel.ochsendorf@gmail.com>
2019-05-31 02:18:01 +0200

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,7 +1,24 @@
 #!bin/bash
-source ~/.bashrc
+
 cd /home/pi/RobotArmWebControl
 #CHECK FOR UPDATE
-python ./src/rpi_setup_scripts/check_for_update.py >> ~/Desktop/update_log.txt
+
+
+ssid = $(iw dev | grep ssid | awk '{print $2}')
+if [ "$ssid" -eq "Keunecke 2" ]; then
+   git pull
+   bash ./src/rpi_setup_scripts/upload_arduino_sketch.sh
+fi
+
+if [ "$ssid" -eq "Keunecke" ]; then
+   git pull
+   bash ./src/rpi_setup_scripts/upload_arduino_sketch.sh
+fi
+
+if [ "$ssid" -eq "FRITZBox7362SL" ]; then
+   git pull
+   bash ./src/rpi_setup_scripts/upload_arduino_sketch.sh
+fi
+
 # RUN FLASK APP
 bash ./src/flask_app/run_flask_app.sh &
