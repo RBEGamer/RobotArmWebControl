5fa12c7c71bf4e6a39465b2134d4d157411622b3
Marcel Ochsendorf <marcel.ochsendorf@gmail.com>
2019-05-31 02:18:49 +0200

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -5,17 +5,17 @@ cd /home/pi/RobotArmWebControl
 
 
 ssid = $(iw dev | grep ssid | awk '{print $2}')
-if [ "$ssid" -eq "Keunecke 2" ]; then
+if [ "$ssid" = "Keunecke 2" ]; then
    git pull
    bash ./src/rpi_setup_scripts/upload_arduino_sketch.sh
 fi
 
-if [ "$ssid" -eq "Keunecke" ]; then
+if [ "$ssid" = "Keunecke" ]; then
    git pull
    bash ./src/rpi_setup_scripts/upload_arduino_sketch.sh
 fi
 
-if [ "$ssid" -eq "FRITZBox7362SL" ]; then
+if [ "$ssid" = "FRITZBox7362SL" ]; then
    git pull
    bash ./src/rpi_setup_scripts/upload_arduino_sketch.sh
 fi
