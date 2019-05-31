a90dd5465f719346eab7c640150beda31d482ad6
Marcel Ochsendorf <marcel.ochsendorf@gmail.com>
2019-05-31 02:24:43 +0200

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -4,21 +4,32 @@ cd /home/pi/RobotArmWebControl
 #CHECK FOR UPDATE
 
 
-ssid = $(iw dev | grep ssid | awk '{print $2}')
-if [ "$ssid" = "Keunecke 2" ]; then
-   git pull
-   bash ./src/rpi_setup_scripts/upload_arduino_sketch.sh
+SSID=`iw dev | grep ssid | awk '{print $2}'`
+echo "$SSID"
+if [ "$SSID" = "Keunecke2" ]; then
+        echo "UP"
+        git pull
+        bash ./src/rpi_setup_scripts/upload_arduino_sketch.sh
 fi
 
-if [ "$ssid" = "Keunecke" ]; then
-   git pull
-   bash ./src/rpi_setup_scripts/upload_arduino_sketch.sh
+if [ "$SSID" = "Keunecke" ]; then
+        echo "UPDATE"
+        git pull
+        bash ./src/rpi_setup_scripts/upload_arduino_sketch.sh
 fi
 
-if [ "$ssid" = "FRITZBox7362SL" ]; then
-   git pull
-   bash ./src/rpi_setup_scripts/upload_arduino_sketch.sh
+if [ "$SSID" = "FRITZBox7362SL" ]; then
+        echo "UPDATE"   
+        git pull
+        bash ./src/rpi_setup_scripts/upload_arduino_sketch.sh
 fi
 
 # RUN FLASK APP
 bash ./src/flask_app/run_flask_app.sh &
+
+
+
+
+
+
+
