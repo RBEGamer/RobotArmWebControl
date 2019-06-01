a46bf2bf383d10daca7a40940746697f8cc082b3
Marcel Ochsendorf <marcel.ochsendorf@gmail.com>
2019-06-01 23:27:38 +0200

    Update setup_base.sh

--- src/rpi_setup_scripts/setup_base.sh
+++ src/rpi_setup_scripts/setup_base.sh
@@ -112,3 +112,9 @@ cd ~/RobotArmWebControl/src/
 arduino-cli board list
 arduino-cli compile --fqbn arduino:sam:arduino_due_x_dbg ./arduino_controller/
 arduino-cli upload -p /dev/ttyACM0 --fqbn arduino:sam:arduino_due_x_dbg ./arduino_controller/
+
+
+
+#SETTING HOSTNAME
+sudo python ~/RobotArmWebControl/src/rpi_setup_scripts/hostname_generator.py
+sudo reboot
