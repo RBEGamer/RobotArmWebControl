73831c10bef7ccfa5bff35b737329c79c61223b0
Marcel Ochsendorf <marcel.ochsendorf@gmail.com>
2019-06-01 23:26:53 +0200

    Update hostname_generator.py

--- src/rpi_setup_scripts/hostname_generator.py
+++ src/rpi_setup_scripts/hostname_generator.py
@@ -1,5 +1,14 @@
 #!bin/python
 
+# INSERT HERE YOUR RPI SERIAL /proc/cpuinfo to automatic get hostname
+serdic = {"c31bbd86-84b3-11e9-bc42-526af7764f64":"rawc1",
+            "c31bbff2-84b3-11e9-bc42-526af7764f64":"rawc12",
+            "c31bc13c-84b3-11e9-bc42-526af7764f64":"rawc3",
+            "c31bc4e8-84b3-11e9-bc42-526af7764f64":"rawc4",
+            "c31bc63c-84b3-11e9-bc42-526af7764f64":"rawc5"
+            ,"c31bc768-84b3-11e9-bc42-526af7764f64":"rawc6"}
+
+
 import subprocess
 from subprocess import Popen, PIPE
 
@@ -25,8 +34,15 @@ hostname = getserial();
 print("RPI_SERIAL")
 print(hostname)
 
+hostname = serdic[str(hostname)]
 
+if not hostname:
+  hostname = "rawc"
   
+print("RPI_NEW_HOSTNAME")
+print(hostname)
+
+
 p1 = Popen(["sudo","hostname", " -b", str(hostname)], stdout=PIPE)
 
 print p1.communicate()
