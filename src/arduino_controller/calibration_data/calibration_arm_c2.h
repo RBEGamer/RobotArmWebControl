db28591ec5e940d24b5318f8d4021c72378dfcca
Marcel Ochsendorf <marcel.ochsendorf@gmail.com>
2019-08-09 14:26:57 +0200

    Update calibration_arm_c2.h

--- src/arduino_controller/calibration_data/calibration_arm_c2.h
+++ src/arduino_controller/calibration_data/calibration_arm_c2.h
@@ -2,14 +2,14 @@
 #define C1_AXIS_1_MIN 0
 #define C1_AXIS_1_MAX 255
 
-#define C1_AXIS_2_MIN 0
-#define C1_AXIS_2_MAX 255
+#define C1_AXIS_2_MIN 35
+#define C1_AXIS_2_MAX 198
 
-#define C1_AXIS_3_MIN 0
-#define C1_AXIS_3_MAX 255
+#define C1_AXIS_3_MIN 91
+#define C1_AXIS_3_MAX 137
 
-#define C1_AXIS_4_MIN 0
-#define C1_AXIS_4_MAX 255
+#define C1_AXIS_4_MIN 154
+#define C1_AXIS_4_MAX 201
 
 #define C1_AXIS_5_MIN 0
 #define C1_AXIS_5_MAX 255
